6a1f0403fe4e89d14fddba2dfea8a78427bf3970
Ismay1l <ismayilovismayil1226@gmail.com>
2024-05-19 11:22:18 -0400

    Initial Commit

---
+++ BrewerApp/BrewerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewerAppApp.swift
+//  BrewerApp
+//
+//  Created by Ismayil Ismayilov on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BrewerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
